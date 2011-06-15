354db736289309293201051111ce6b78aa9d96d9
andrewjpage <andrewjpage@gmail.com>
2011-06-15 11:48:11 +0100

    Display 503 if delayed jobs too old

--- delayed_job_monitor.rb
+++ delayed_job_monitor.rb
@@ -9,6 +9,8 @@ require 'lib/barcode'
 APP_ROOT = File.dirname(File.expand_path(__FILE__))
 RAILS_ENV = (ENV['RAILS_ENV'] ||= 'development')
 @@database = YAML::load(File.open( File.join(APP_ROOT,'config/database.yml') ))
+DELAYED_JOB_HTTP_ERROR= 503
+DELAYED_JOB_HTTP_OK = 200
 
 helpers do
   def get_version_string
@@ -16,34 +18,73 @@ helpers do
     Deployed::VERSION_STRING
   end
   
-  def generate_page(application_name, max_pending_jobs, max_failed_jobs)
-      DelayedJob.establish_connection(
-        @@database["#{RAILS_ENV}_#{application_name}"]
-      )
-      number_of_jobs = DelayedJob.count
-      number_of_failed_jobs = DelayedJob.find(:all, :conditions => [ 'last_error is not NULL' ]).count
+  def delayed_job_connection(application_name)
+    DelayedJob.establish_connection(
+      @@database["#{RAILS_ENV}_#{application_name}"]
+    )
+  end
+  
+  def generate_default_status_page(application_name, max_pending_jobs, max_failed_jobs, max_delayed_job_age_in_days)
+      delayed_job_connection(application_name)
 
-      status 503 if (number_of_jobs > max_pending_jobs || number_of_failed_jobs > max_failed_jobs)
-      content_type 'text/plain', :charset => 'utf-8'
-      <<-_EOF_
-#{DelayedJob.count} Delayed jobs pending
+      if (number_of_delayed_jobs > max_pending_jobs || number_of_failed_jobs > max_failed_jobs || number_of_old_delayed_jobs(max_delayed_job_age_in_days) > 0)
+        http_status_code =  DELAYED_JOB_HTTP_ERROR 
+      else
+        http_status_code = DELAYED_JOB_HTTP_OK
+      end
+      
+      render_page(http_status_code, "#{number_of_delayed_jobs} jobs pending")
+  end
+  
+  def generate_warehouse_status_page(application_name, max_delayed_job_age_in_days)
+    delayed_job_connection(application_name)
+
+    number_of_old_jobs = number_of_old_delayed_jobs(max_delayed_job_age_in_days)
+    
+    if number_of_old_jobs > 0
+      http_status_code =  DELAYED_JOB_HTTP_ERROR 
+    else
+      http_status_code = DELAYED_JOB_HTTP_OK
+    end
+      
+    render_page(http_status_code, "#{number_of_delayed_jobs} jobs pending")
+  end
+  
+  def number_of_old_delayed_jobs(max_delayed_job_age_in_days)
+    DelayedJob.count(:conditions => ["created_at < SUBDATE(NOW(), INTERVAL #{max_delayed_job_age_in_days} DAY)"])
+  end
+  
+  def number_of_delayed_jobs
+    DelayedJob.count
+  end
+  
+  def number_of_failed_jobs
+    DelayedJob.count(:conditions => [ 'last_error is not NULL' ])
+  end
+  
+  def render_page(http_status_code, text_to_render)
+    status http_status_code
+    content_type 'text/plain', :charset => 'utf-8'
+    <<-_EOF_
+    #{text_to_render}
     _EOF_
   end
+  
 end
 
 class DelayedJob < ActiveRecord::Base
 end
 
 get '/sequencescape' do
-  generate_page('sequencescape', 8, 0)
+  generate_default_status_page('sequencescape', 8, 0, 1)
 end
 
 get '/process_tracking' do
-  generate_page('process_tracking', 8, 0)
+  generate_default_status_page('process_tracking', 8, 0, 1)
 end
 
 get '/warehouse_two' do
-  generate_page('warehouse_two', 4000, 10)
+  generate_warehouse_status_page('warehouse_two', 2 )
 end
 
 get '/' do
